af28c19bc6e1bb8fcc17db114d0de6a885241847
mdemers <mdemers@hagley.org>
2023-09-27 09:20:45 -0400

    pre-341 setup

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -1,1 +1,11 @@
 Plugins::add_record_page_action_erb(['resource', 'archival_object'], 'shared/contact_us', 0)
+Rails.application.config.after_initialize do
+  class WelcomeController < ApplicationController
+    def show
+     @page_title = I18n.t 'brand.welcome_page_title'
+     @fa_counts = archivesspace.get_types_counts('pui_collection')
+     @search = Search.new(params)
+     render
+    end
+  end
+end
